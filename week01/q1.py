8552d9a2212dda93fbb0c62eab7094b16fc7e149
Hammad564 <91627884+Hammad564@users.noreply.github.com>
2024-08-29 15:07:01 +0530

    Add files via upload

---
+++ week01/q1.py
@@ -0,0 +1,9 @@
+num1=int(input("Enter number 1 :"))
+num2=int(input("Enter number 2 :"))
+
+product=num1*num2
+sum=num1+num2
+if(product<=5000):
+    print("Sum is :"+sum)
+else:
+    print("Product is greater than 5000")
